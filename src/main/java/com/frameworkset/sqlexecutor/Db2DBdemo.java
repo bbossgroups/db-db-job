82e45e3141b863fba1684bbc7051d5c524a5523f
yin-bp <yin-bp@163.com>
2023-03-28 21:42:09 +0800

    整理案例

--- src/main/java/com/frameworkset/sqlexecutor/Db2DBdemo.java
+++ src/main/java/com/frameworkset/sqlexecutor/Db2DBdemo.java
@@ -268,10 +268,7 @@ public class Db2DBdemo implements InitializingBean {
 				logger.info(taskMetrics.toString());
 			}
 
-			@Override
-			public int getMaxRetry() {
-				return 0;
-			}
+
 		});
 		/**
 		 * 执行数据库表数据导入es操作
